e5250c315b6679f64433c94e41e6e9a5aa626a2c
stefko-tsonyovski-fntext <stefko.tsonyovski.ext@fntsoftware.com>
2024-01-29 15:48:35 +0200

    Added JdonPropertyName attributes

--- PayloadDto.cs
+++ PayloadDto.cs
@@ -1,9 +1,13 @@
-﻿namespace PWAPushNotificationsServer
+﻿using System.Text.Json.Serialization;
+
+namespace PWAPushNotificationsServer
 {
     public class PayloadDto
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
 
+        [JsonPropertyName ("message")]
         public string Message { get; set; }
     }
 }
